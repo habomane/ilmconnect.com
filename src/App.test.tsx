0fdf9f7a53e686e1a6c8ab90bb51122464b8cf6f
Anum1608 <riaz.anum@outlook.fr>
2024-11-18 12:15:46 +0000

    added Vitest unit testing framework and one test (passing)

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+// To Test
+import App from "./App";
+
+// Tests
+describe("Renders main page correctly", async () => {
+    it("Should render the page correctly with the text Hello", async () => {
+    // Setup
+    render(<App />);
+    const h1 = await screen.queryByText("Hello");
+
+    // Expectations
+    expect(h1).toBeInTheDocument();
+    });
+});
