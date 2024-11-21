4ec5be50bf710992e96df6166423fedd007315a5
Anum1608 <riaz.anum@outlook.fr>
2024-11-21 11:29:44 +0000

    implemented header with log in signup buttons + log out button only when user is logged in

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,43 @@
 import { Text, TextType } from "@/components/typography/Text";   
 import { useState } from "react"
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => {
     const [type] = useState(TextType.headerOneBlack);
+
+    // temporary parameter for testing until logged in state becomes available
+    let isLoggedIn = 0; 
+
     return (
-        <div className="flex justify-center items-center">
+    <div className="flow-root justify-center bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 shadow">
+
+        <div className="float-left">
             <Text type={type}>IlmConnect</Text>
         </div>
+
+        <div className="float-right">
+            {isLoggedIn ? 
+                (   
+                    <button type="button" className="rounded-full bg-customPurple px-8 py-2.5" > 
+                        <Text type={TextType.headerThreeWhite}>Log out</Text> 
+                    </button>
+                ):(
+                    <>
+                        <Link to="/login">
+                        <button type="button" className="rounded-full bg-customPurple px-8 py-2.5"> 
+                            <Text type={TextType.headerThreeWhite}>Login</Text>
+                        </button>
+                        </Link>
+
+                        <Link to="/signup">
+                        <button type="button" className="rounded-full bg-white border border-black px-8 py-2.5"> 
+                            <Text type={TextType.headerThreeBlack}>Sign up</Text>
+                        </button>
+                        </Link>
+                    </>
+                )
+            }
+        </div>
+    </div>
     )
 }
