56b4f159271312494c991095233393106a2401fc
Anum1608 <riaz.anum@outlook.fr>
2024-11-22 12:56:28 +0000

    created search bar component

---
+++ src/components/searchBar/SearchBar.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const SearchBar: React.FC = () => {
+    const [query,setQuery]=useState("")
+
+    const handleChange = (value: string) => {
+        setQuery(value);
+        // fetch the api with the query and get the filtered the results
+        // navigate to another page to show results
+    }
+
+    return ( 
+            <div className="search_bar_input">
+                <input 
+                    placeholder="Type to search mentors"
+                    type="text"
+                    onChange={e=>handleChange(e.target.value)}
+                />     
+            </div>)
+}
